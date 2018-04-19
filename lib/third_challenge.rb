dc765f37eab02367e8508aed29ddbcab4cfe895c
Marie Sonko <mariesonko62@gmail.com>
2018-04-19 18:27:35 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,16 +1,16 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {:name => {}, :age => {}
+      :patriarch => {name : "Lord Montague", age : "53"
         #your key/value pairs here
         },
-      :matriarch => {:name => {}, :age => {}
+      :matriarch => {name : "Lady Montague" , age : "54"
         #your key/value pairs here
         },
-      :hero => { :name => {}, :status => {}
+      :hero => { name : "Romeo", age : "15",  status : "alive"
         #your key/value pairs here
         },
-      :hero_friends => [:friend1 => {}, :friend2 => {}]
+      :hero_friends => []
    },
    :capulet => {
       :patriarch => {:name => {}, :age => {}
