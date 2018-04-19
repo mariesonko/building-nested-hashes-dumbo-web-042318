89034b4deb598d845a824f5868f1467cd7d87159
Marie Sonko <mariesonko62@gmail.com>
2018-04-19 17:59:47 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,5 +1,5 @@
 def first_challenge
-   epic_tragedy = { :montague => {"Patriarch", "Matriarch", "Hero", "Hero's Friends" }
+   epic_tragedy = { :montague => {"patriarch", "matriarch", "hero", "hero's friends" }
      :capulet => {}
    }
 end
